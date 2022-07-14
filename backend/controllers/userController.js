c3befb92034f499ee07d57972c5119517b0eec25
santikaagustianti <191240000822@unisnu.ac.id>
2022-07-14 13:19:46 +0700

    Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,48 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+
 // @desc    Register user
 // @route   POST /api/goals
 // @access  Public
-const registerUser = (req, res) => {
-    res.json({message: "Register User"})
-}
+const registerUser = asyncHandler(async(req, res) => {
+
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+
+})
 
 
 // @desc    Login user
 // @route   POST /api/goals
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     res.json({message: "Login User"})
-}
+})
 
 
 // @desc    Get User user
 // @route   POST /api/goals
 // @access  Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async(req, res) => {
     res.json({message: "User data display"})
-}
+})
 
 
 
