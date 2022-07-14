7b01292c6dcafa138e7bed3b3c1f7a6d487d33c9
santikaagustianti <191240000822@unisnu.ac.id>
2022-07-14 13:59:19 +0700

    const express = require('express')

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -4,10 +4,14 @@ const {
     getGoals, 
     setGoal, 
     updateGoal, 
-    deleteGoal,
-} = require('../controllers/goalController')
+    deleteGoal } = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal) 
 
-module.exports = router 
+const {protect} = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal) 
+
+
+
+module.exports = router
