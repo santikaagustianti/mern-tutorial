c61dddae4a65ca6ed254554272a0a6182308aed7
santikaagustianti <191240000822@unisnu.ac.id>
2022-07-14 13:32:50 +0700

    Authenticate User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async(req,res) => {
             name: user.name,
             email: user.email,
         })
+        
     } else {
         res.status(400)
         throw Error('Invalid user data')
@@ -47,8 +48,26 @@ const registerUser = asyncHandler(async(req,res) => {
 // @desc    Login user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = asyncHandler(async(req,res) => {
-    res.json({ message: "Login User"})
+const loginUser = asyncHandler(async(req, res) => {
+    
+    const {email, password} = req.body
+
+    // Check for user email
+    const user = await User.findOne({email})
+    
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+           
+        })
+
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+   
 })
 
 // @desc    Get user data
